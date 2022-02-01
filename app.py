f1c5027b00442ea08c98c6dc647860a1afa4ef61
Sunny Seera <sunnyseera@hotmail.co.uk>
2022-02-01 20:40:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 import os
 from flask import Flask, jsonify
+import requests
+from google.cloud import secretmanager_v1
 
 app = Flask(__name__)
 
+# Create Secert Name
+secret_name = "my_awesome_secret_from_secret_manager"
+
+# Create CLient
+secret_manager_client = secretmanager_v1.SecretManagerServiceClient()
+
+
+def get_numeric_project_id():
+    METADATA_URL = 'http://metadata.google.internal/computeMetadata/v1/'
+    METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
+
+    try:
+        url = METADATA_URL + 'project/numeric-project-id'
+        resp = requests.get(
+            url,
+            headers=METADATA_HEADERS)
+        print("Project id is {}".format(resp.text))
+        return resp.text
+    except requests.exceptions.ConnectionError:
+        return "XXX"
+
 
 @app.route("/", methods=["GET"])
-def hello_world():
-    return jsonify(message="hello world"), 200
+def read_secrets():
+
+    my_secret = secret_manager_client.access_secret_version(
+        name=f"projects/{get_numeric_project_id()}/secrets/{secret_name}/versions/latest"
+    )
+
+    my_secret_str = my_secret.payload.data.decode('UTF-8')
+
+    return jsonify(my_awesome_secret_from_secret_manager=f"{my_secret_str}"), 200
 
 
 if __name__ == '__main__':
